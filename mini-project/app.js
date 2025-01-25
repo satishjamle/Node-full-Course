f70be7f92c8db1b13b23caaf9ca693fc9406e204
satishjamle <satishjamle513@gmail.com>
2025-01-25 01:42:59 -0800

    mini Project -11 (  todo -1)

--- mini-project/app.js
+++ mini-project/app.js
@@ -1,1 +1,45 @@
-import readline from "readline"
+import readline from "readline";
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+})
+
+
+const todos = [];
+
+const showMenu = () => {
+    console.log("\n1: Add a Task")
+    console.log("2: View Tasks ")
+    console.log("3: Exit ")
+
+    rl.question("Choose an option:", handleInput)
+}
+const handleInput = (option) => {
+    if (option === "1") {
+        rl.question("enter the Task :", (task)  => {
+            todos.push(task);
+            console.log("Task added :", task);
+            showMenu()
+        })
+    }
+    else if (option === "2") {
+        console.log("\n Your Todo Lists");
+        todos.forEach((task ,index) => {
+            console.log(`${index+1}.${task}`);
+            
+        })
+        showMenu()
+    }
+    else if(option === "3"){
+        console.log("Good Byee")
+        rl.close()
+    }
+    else{
+        console.log("invaild Option Please try again")
+        showMenu(); 
+    }
+}
+
+
+showMenu() 
