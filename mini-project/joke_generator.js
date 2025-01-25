8360f4f6fb6b31c755cc47a15fffd470664450c4
satishjamle <satishjamle513@gmail.com>
2025-01-25 05:37:41 -0800

    mini project - 11 ( joke-generator -03)

---
+++ mini-project/joke_generator.js
@@ -0,0 +1,29 @@
+import https from 'https';
+import chalk from 'chalk';
+
+const getJoke = () => {
+    const url = 'https://official-joke-api.appspot.com/random_joke';
+
+    https.get(url, (response) => {
+        let data = "";
+
+        response.on("data", (chunk) => {
+            data += chunk;
+        });
+
+        response.on('end', () => {
+
+            const joke = JSON.parse(data);
+            console.log(chalk.green(`Here is a random ${joke.type} joke:`));
+            console.log(chalk.red(joke.setup));
+            console.log(chalk.blue.bgRed.bold(joke.punchline));
+
+        });
+
+        response.on('error', (err) => {
+            console.log(`Error fetching the joke: ${err.message}`);
+        });
+    });
+};
+
+getJoke();
